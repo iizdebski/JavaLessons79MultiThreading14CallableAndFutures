ebcdd19708d0592060dee91db7b11d12b62a6d64
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-31 15:57:34 +0300

    Executors

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Callable<Integer> callable = new MyCallable();
+        FutureTask futureTask = new FutureTask(callable);
+        new Thread(futureTask).start();
+        System.out.println(futureTask.get());
+    }
+
+    static class MyCallable implements Callable<Integer> {
+        @Override
+        public Integer call() throws Exception {
+            int j = 0;
+            for (int i = 0; i < 10; i++, j++ ) {
+                Thread.sleep(500);
+            }
+            return j;
+        }
+    }
+}
